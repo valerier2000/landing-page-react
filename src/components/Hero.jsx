75ebe55bb9d1db310c1a04433934636ff6fad0c0
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2025-01-30 21:45:18 +0100

    Create Hero component with description

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+function Hero() {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold p-2">
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+          Grow with data.
+        </h1>
+        <div>
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+            Fast, flexible financing for
+          </p>
+        </div>
+        <p>
+          Monitor your data analytics to increase revenue for BTB, BTC, & SASS
+          platforms.
+        </p>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
