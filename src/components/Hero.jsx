ffb612c965d4a716def7bd5180627ee5f0a56139
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2025-01-30 22:27:42 +0100

    Finish styling Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,22 +11,24 @@ function Hero() {
           Grow with data.
         </h1>
         <div className="flex justify-center items-center">
-          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
             Fast, flexible financing for
           </p>
           <ReactTyped
-            className="md:text-5xl sm:text-4xl text-xl font-bold pl-2"
+            className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
             strings={["BTB", "BTC", "SASS"]}
             typeSpeed={120}
             backSpeed={140}
             loop
           />
         </div>
-        <p>
+        <p className="md:text-2xl text-xl font-bold text-gray-500">
           Monitor your data analytics to increase revenue for BTB, BTC, & SASS
           platforms.
         </p>
-        <button>Get Started</button>
+        <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
+          Get Started
+        </button>
       </div>
     </div>
   );
